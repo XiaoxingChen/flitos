7438d58ca5d734f6fa4801f7e410876d396f214b
XiaoxingChen <chen-xx@zju.edu.cn>
2022-12-17 16:15:20 +0800

    fix: qemu hello world, wrap with ifdef __cplusplus.

--- riscv_qemu_hello_world/src/main.cpp
+++ riscv_qemu_hello_world/src/main.cpp
@@ -7,12 +7,15 @@ int main() {
     int a = 4;
     int b = 12;
     char * UART_MEMORY = (char*)(0x10000000);
+    uint16_t * VIRT_TEST = (uint16_t*)(0x10'0000);
 
     char msg[] = "hello world\n";
     for(int i = 0; i < sizeof(msg); i++)
     {
         UART_MEMORY[0] = msg[i];
     }
+
+    // VIRT_TEST[0] = 0x5555;
 
     return a + b;
 }
